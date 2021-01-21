a34780994b63df9a81385b057cc1cee3e62425e4
CindyKatoni <katmuema@gmail.com>
2021-01-21 14:57:49 +0300

    Customer model

--- store/models.py
+++ store/models.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.contrib.auth.models import User 
 # Create your models here.
+
+class Customer(models.Model):
+    #One to one relationship means that a user can only have one customer and a customer can only be one user
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+
+    def__str__(self):
+        return self.name
