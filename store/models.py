a5b9c0909ce29bea3b01110aa453467c6fbb29df
CindyKatoni <katmuema@gmail.com>
2021-01-21 18:01:31 +0300

    Created shipping model

--- store/models.py
+++ store/models.py
@@ -35,4 +35,21 @@ class Order(models.Model):
         return str(self.id)
 
 #OrderItem will have individual product items..
-        
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def__str__(self):
+        return str(self.id)
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def__str__(self):
+        return self.address
