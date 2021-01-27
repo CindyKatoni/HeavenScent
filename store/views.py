ca87dbb2622f539e740d180dc73f00d8f6472c6a
CindyKatoni <katmuema@gmail.com>
2021-01-27 12:20:01 +0300

    add updateItem view

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import *
 
 # Create your views here.
@@ -34,3 +35,7 @@ def checkout(request):
 
     context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+    return JsonResponse('Item added successfully', safe=False)    
