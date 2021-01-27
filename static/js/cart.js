6cbfb78b68b69a335104dc6baa16d2e5a5a1a452
CindyKatoni <katmuema@gmail.com>
2021-01-27 12:54:32 +0300

    return promise response to fetch

--- static/js/cart.js
+++ static/js/cart.js
@@ -31,5 +31,12 @@ function updateUserOrder(productId, action) {
         body: JSON.stringify({ 'productId': productId, 'action': action })
     })
 
+    .then((response) => {
+        return response.json()
+    })
+
+    .then((data) => {
+        console.log('data:', data)
+    })
 
 }
