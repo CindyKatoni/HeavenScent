393cb46054f33ffab564cb7b5c1641e732e5b366
CindyKatoni <katmuema@gmail.com>
2021-01-28 19:07:52 +0300

    process order get cart total

--- store/views.py
+++ store/views.py
@@ -82,5 +82,17 @@ def updateItem(request):
 
 def processOrder(request):
     transaction_id = datetime.datetime.now().timestamp()
-    print('Data:', request.body)
+    data = json.loads(request.body)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total ==  order.get_cart_total:
+            order.complete = True
+        order.save()    
+    else:
+        print("User is not logged in")
+    
     return JsonResponse("Payment submitted...", safe=False)    
