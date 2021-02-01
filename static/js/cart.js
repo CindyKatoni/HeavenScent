3bdde48f2a791a3dfd005f862eda6bd67ab06f46
CindyKatoni <katmuema@gmail.com>
2021-02-01 14:21:33 +0300

    add and remove action for guest user products

--- static/js/cart.js
+++ static/js/cart.js
@@ -9,13 +9,37 @@ for (var i = 0; i < updateBtns.length; i++) {
 
         console.log('USER:', user)
         if (user === "AnonymousUser") {
-            console.log("Not logged in")
+            addCookieItem()
         } else {
             updateUserOrder(productId, action)
                 // console.log("User is logged in. Sending data...")
         }
     })
 }
+
+function addCookieItem(productId, action) {
+    console.log('Not logged in..')
+    if (action == 'add') {
+        if (cart[productId] == undefined) {
+            cart[productId] = { 'quantity': 1 }
+        } else {
+            cart[productId]['quantity'] + 1
+        }
+    }
+    if (action == 'remove') {
+        cart[productId]['quantity'] -= 1
+
+        if (cart[productId]['quantity'] <= 0) {
+            console.log('Remove Item')
+            delete cart[productId]
+        }
+    }
+}
+
+
+
+
+
 
 function updateUserOrder(productId, action) {
     console.log("User is authenticated. Sending data...")
