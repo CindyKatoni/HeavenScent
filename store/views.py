092d6ab3d72fd87b37224164cfed3041b4912c28
CindyKatoni <katmuema@gmail.com>
2021-02-10 15:57:58 +0300

    update the store navbar with the cookieCart function

--- store/views.py
+++ store/views.py
@@ -20,9 +20,8 @@ def store(request):
             items = order.orderitem_set.all()
             cartItems = order.get_cart_items
         else:
-            items = []
-            order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
-            cartItems = order['get_cart_items']
+            cookieData = cookieCart(request)
+            cartItems = cookieData['cartItems']
 
         products = Product.objects.all()
         context = {'products': products, 'cartItems': cartItems}
